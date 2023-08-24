db2fa3e7199123c13c9f821551b356683554d2ec
sawacha emmanuel <sawachaemmanuel@gmail.com>
2023-08-24 16:17:43 -0700

    change

--- tet.c
+++ tet.c
@@ -1,13 +1,18 @@
+#include <limits.h>
 #include <stdio.h>
 #include "main.h"
-#include <limits.h>
 
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	int len;
-	int len2;
-	unsigned int ui;
-	void *addr;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
     len = _printf("Let's try to printf a simple sentence.\n");
     len2 = printf("Let's try to printf a simple sentence.\n");
